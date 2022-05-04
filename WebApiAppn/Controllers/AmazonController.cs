e272fb98c17b4286df168096f6046a68cc112c9d
KavyaDodda <104917434+KavyaDodda@users.noreply.github.com>
2022-05-04 21:26:58 +0530

    Uploaded

---
+++ WebApiAppn/Controllers/AmazonController.cs
@@ -0,0 +1,131 @@
+﻿using BusinessLayer2;
+using DomainLayer2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAppn.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AmazonController : ControllerBase
+    {
+       
+
+        private readonly ILogger<AmazonController> _logger;
+        private readonly IOrderBusiness _orderBusiness;
+
+
+        public AmazonController(ILogger<AmazonController> logger, IOrderBusiness orderBusiness)
+        {
+            _logger = logger;
+            _orderBusiness = orderBusiness;
+
+         }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<AmazonDto>))]
+        [Route("get-allAmazonCountries")]
+        public async Task<ActionResult<IEnumerable<AmazonDto>>> GetAll()
+        {
+            try
+            {
+                var resp = await _orderBusiness.GetAllAmazonCountries();
+
+                if (resp == null || resp.Count == 0)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(AmazonDto))]
+        [Route("get-amazonCountryById")]
+        public async Task<ActionResult<AmazonDto>> GetAmazonCountryById(int id)
+        {
+            try
+            {
+                var resp = await _orderBusiness.GetAmazonCountryById(id);
+
+                if (resp == null)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("insert-amazonCountry")]
+        public async Task<ActionResult> InsertEmployee([FromBody] AmazonDto amazon)
+        {
+            try
+            {
+                await _orderBusiness.InsertAmazonCountry(amazon);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("update-amazonCountry")]
+        public async Task<ActionResult> UpdateAmazonCountry([FromBody] AmazonDto amazon)
+        {
+            try
+            {
+                await _orderBusiness.UpdateAmazonCountry(amazon);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("delete-amazonCountryById")]
+        public async Task<ActionResult> DeleteAmazonCountryById(int id)
+        {
+            try
+            {
+                await _orderBusiness.DeleteAmazonCountryById(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ WebApiAppn/Controllers/OrderController.cs
@@ -0,0 +1,183 @@
+﻿using BusinessLayer2;
+using DomainLayer2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAppn.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderBusiness _orderBusiness;
+
+
+        public OrderController(ILogger<OrderController> logger, IOrderBusiness orderBusiness)
+        {
+            _logger = logger;
+            _orderBusiness = orderBusiness;
+
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<OrderDto>))]
+        [Route("get-allOrders")]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders()
+        {
+            try
+            {
+                var resp = await _orderBusiness.GetAllOrders();
+
+                if (resp == null || resp.Count == 0)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(OrderDto))]
+        [Route("get-orderById")]
+        public async Task<ActionResult<OrderDto>> GetOrderById(int Id)
+        {
+            try
+            {
+                var resp = await _orderBusiness.GetOrderById(Id);
+
+                if (resp == null)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("insert-order")]
+        public async Task<ActionResult> InsertOrder([FromBody] OrderDto orderDto)
+        {
+            try
+            {
+                await _orderBusiness.InsertOrder(orderDto);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("update-order")]
+        public async Task<ActionResult> UpdateOrder([FromBody] OrderDto orderDto)
+        {
+            try
+            {
+                await _orderBusiness.UpdateOrder(orderDto);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("delete-orderById")]
+        public async Task<ActionResult> DeleteOrderById(int Id)
+        {
+            try
+            {
+                await _orderBusiness.DeleteOrderById(Id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<OrderDto>))]
+        [Route("get-all-orders-by-country-name")]
+        public async Task<ActionResult<List<OrderDto>>> GetAllOrdersByCountryName(string name)
+        {
+            try
+            {
+
+
+
+
+                var resp = await _orderBusiness.GetAllOrdersByCountryByName(name);
+
+
+
+
+                if (resp == null || resp.Count == 0)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<OrderDto>))]
+        [Route("get-sum-of-orders-cost-by-country-name")]
+        public async Task<ActionResult<double>> SumOfOrdersCostByCountryName(string name)
+        {
+            try
+            {
+
+                var resp = await _orderBusiness.SumOfOrdersCostByCountryName(name);
+
+                return Ok(resp);
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
